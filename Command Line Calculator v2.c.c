f6f2a5b29f81630bafca1ed5a5bb560f2caf5b73
Faraan_R <48865049+WWFYOcelot@users.noreply.github.com>
2023-01-03 20:08:52 -0500

    Update and rename clc_v3.c to Command Line Calculator v2.c.c

--- Command Line Calculator v2.c.c
+++ Command Line Calculator v2.c.c
@@ -1,9 +1,3 @@
-/*
-COMP-1400 Assignment 3
-Faraan Rashid 
-110101889
-*/
-
 #include <stdio.h> // C standard Library for input/output functions
 #include <math.h>  // C math library for math functions 
 
